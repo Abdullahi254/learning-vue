499d6e754288b5296141298a0d0e88874a46e8c8
Abdullahi254 <abdush504@gmail.com>
2024-04-23 14:06:44 +0300

    separating components to respective functionality

---
+++ src/components/NavbarLink.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    props: ["link", "isActive"],
+    computed: {
+        activeClasses() {
+            return {
+                active: this.isActive,
+                emphasize: this.isActive
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <a class="nav-link" :href="link.link" :title="`this is a link to ${link.text}`" :class="activeClasses">
+        {{ link.text }}
+    </a>
+</template>
+
+<style scoped>
+.emphasize{
+    text-decoration: underline !important;
+}
+</style>
