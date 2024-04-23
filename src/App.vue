118292f80923e5e9fd4d40bb8dfa62d1abf901be
Abdullahi254 <abdush504@gmail.com>
2024-04-23 20:31:07 +0300

    handling form data

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
     <navbar :pages="pages" :active-index="activeIndex" :change-active="(index) => activeIndex = index"></navbar>
-    <page-viewer v-if="pages.length > 0" :page="pages[activeIndex]"></page-viewer>
+    <!-- <page-viewer v-if="pages.length > 0" :page="pages[activeIndex]"></page-viewer> -->
+    <login :handle-submit="handleSubmit"></login>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue"
 import PageViewer from "./components/PageViewer.vue"
+import Login from "./components/Login.vue"
 export default {
     components: {
         PageViewer,
-        Navbar
+        Navbar,
+        Login
     },
     created(){
         this.getPages()
@@ -25,6 +28,10 @@ export default {
             const res = await fetch("pages.json")
             const data = await res.json()
             this.pages = data.pages
+        },
+        handleSubmit(email, pass){
+            console.log(email)
+            console.log(pass)
         }
     }
 }
---
+++ src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <form>
+            <div class="form-group mb-3 mt-3">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                    placeholder="Enter email" v-model="emailData">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
+                    else.</small>
+            </div>
+            <div class="form-group mb-3">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"
+                    v-model="passData">
+            </div>
+            <div class="form-check mb-2">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+            </div>
+            <button type="submit" class="btn btn-primary"
+                @click.prevent="() => handleSubmit(emailData, passData)">Submit</button>
+        </form>
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: ["handleSubmit"],
+    data() {
+        return {
+            emailData: '',
+            passData: ''
+        }
+    }
+}
+</script>
