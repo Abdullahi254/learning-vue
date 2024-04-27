e1bf84beaf6b4e79c3f1a27558eb1ad55460aaa2
Abdullahi254 <abdush504@gmail.com>
2024-04-28 01:45:38 +0300

    introduction to composition api

--- src/views/PageList.vue
+++ src/views/PageList.vue
@@ -1,3 +1,16 @@
 <template>
     <p>List of pages will be here</p>
+    <p>Count: {{ count }}</p>
+    <button @click.prevent="click">Increase(+)</button>
 </template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    const count = ref(0)
+
+    const click = ()=>{
+        count.value++;
+    }
+
+</script>
